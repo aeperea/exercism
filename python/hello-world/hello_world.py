7f46cd254b5d891d379728df7900c3ff44613188
aeperea <elizondoperea@gmail.com>
2016-08-20 16:14:12 -0500

    Add python hello world

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,4 @@
+from __future__ import unicode_literals
+
+def hello(name='World'):
+    return "Hello, {0}!".format(name)
--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,6 @@
 class Gigasecond
   def self.from(time)
-    time + 1e9.to_i
+    time + 1e9
   end
 end
 

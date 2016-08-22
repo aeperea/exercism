ca75974e18c1d361e56b047b14841354ea780c98
aeperea <elizondoperea@gmail.com>
2016-08-21 22:42:34 -0500

    Solution to series

---
+++ ruby/series/series.rb
@@ -0,0 +1,17 @@
+require 'awesome_print'
+
+class Series
+  def initialize(str)
+    @str = str
+  end
+
+  def slices(num_of_slices)
+    str_arr = @str.chars
+    size    = str_arr.size
+    raise ArgumentError if size < num_of_slices || num_of_slices < 1
+    str_arr.map.with_index do |char, i|
+      next if (i + num_of_slices - 1) >= (size)
+      (i..(i + num_of_slices - 1)).to_a.map { |j| str_arr[j].to_i }
+    end.compact
+  end
+end

775db9c588eb87ad46cbb210984a3c3e2e4207b0
aeperea <elizondoperea@gmail.com>
2016-08-21 22:48:44 -0500

    Better solution using each_cons

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,15 +1,10 @@
 class Series
   def initialize(str)
-    @str = str
+    @str = str.chars.map(&:to_i)
   end
 
   def slices(num_of_slices)
-    str_arr = @str.chars
-    size    = str_arr.size
-    raise ArgumentError if size < num_of_slices || num_of_slices < 1
-    str_arr.map.with_index do |char, i|
-      next if (i + num_of_slices - 1) >= (size)
-      (i..(i + num_of_slices - 1)).to_a.map { |j| str_arr[j].to_i }
-    end.compact
+    raise ArgumentError if num_of_slices > @str.length
+    @str.each_cons(num_of_slices).to_a
   end
 end

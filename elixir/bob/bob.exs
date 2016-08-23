99d37122887d2940e163b3998ac404880cda91f7
aeperea <elizondoperea@gmail.com>
2016-08-23 00:55:55 -0500

    Made bob a little more elixiry

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -8,26 +8,27 @@ defmodule Bob do
   }
 
   def hey(input) do
-    res = @phrases[:whatever]
-    if is_question?(input), do: res = @phrases[:question]
-    if is_yelling?(input),  do: res = @phrases[:yell]
-    if is_silence?(input),  do: res = @phrases[:silence]
-    res
+    cond do
+      is_question?(input) -> @phrases[:question]
+      is_yelling?(input) ->  @phrases[:yell]
+      is_silence?(input) ->  @phrases[:silence]
+      true -> @phrases[:whatever]
+    end
   end
 
-  def is_yelling?(input) do
+  defp is_yelling?(input) do
     input == String.upcase(input) && not is_question?(input) && not only_nums_and_chars(input)
   end
 
-  def only_nums_and_chars(input) do
+  defp only_nums_and_chars(input) do
     not Regex.match?(~r/[[:alpha:]]/u, input)
   end
 
-  def is_question?(input) do
+  defp is_question?(input) do
     Regex.match?(~r/\?$/, input)
   end
 
-  def is_silence?(input) do
+  defp is_silence?(input) do
     not Regex.match?(~r/[[:alnum:]]/u, input)
   end
 

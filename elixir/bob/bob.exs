4712f1da652af05a1bd795739a76c08cfcc120c1
aeperea <elizondoperea@gmail.com>
2016-08-23 00:41:18 -0500

    Add bob in elixir

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,17 +1,34 @@
 defmodule Bob do
-  def hey(input) do
-    cond do
-        true -> raise "Your implementation goes here"
 
-    end
+  @phrases %{
+    yell:      "Whoa, chill out!",
+    question:  "Sure.",
+    silence:   "Fine. Be that way!",
+    whatever:  "Whatever."
+  }
+
+  def hey(input) do
+    res = @phrases[:whatever]
+    if is_question?(input), do: res = @phrases[:question]
+    if is_yelling?(input),  do: res = @phrases[:yell]
+    if is_silence?(input),  do: res = @phrases[:silence]
+    res
   end
 
   def is_yelling?(input) do
-    input == String.upcase(input)
+    input == String.upcase(input) && not is_question?(input) && not only_nums_and_chars(input)
   end
 
-  def is_a_question?(input) do
+  def only_nums_and_chars(input) do
+    not Regex.match?(~r/[[:alpha:]]/u, input)
+  end
 
+  def is_question?(input) do
+    Regex.match?(~r/\?$/, input)
+  end
+
+  def is_silence?(input) do
+    not Regex.match?(~r/[[:alnum:]]/u, input)
   end
 
 end

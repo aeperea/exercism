fc6804a149d291991b4def4f2f7d4017a40827d6
aeperea <elizondoperea@gmail.com>
2016-08-24 13:11:34 -0500

    Cleaner methods

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,30 +1,21 @@
 class Clock :
     def __init__(self, hour, minutes) :
-        self.clean_hour_and_minutes(hour, minutes)
+        self._hour    = hour
+        self._minutes = minutes
+        self.clean_hour_and_minutes()
 
     def __str__(self) :
-        return "{0}:{1}".format(self.add_zero(self._hour), self.add_zero(self._minutes))
+        return "%02d:%02d" % (self._hour, self._minutes)
 
     def __eq__(self, other) :
         return self._hour == other._hour and self._minutes == other._minutes
 
-    def add_zero(self, num) :
-        if num >= 10 :
-            return str(num)
-        else :
-            return "0" + str(num)
-
     def add(self, minutes) :
-        new_minutes  = self._minutes + minutes
-        self.clean_hour_and_minutes(self._hour, new_minutes)
+        self._minutes += minutes
+        self.clean_hour_and_minutes()
         return self
 
-    def clean_hour_and_minutes(self, hour, minutes) :
-        hour   += minutes / 60
-        minutes = minutes % 60
-        hour    = hour % 24
-
-        self._minutes = minutes
-        self._hour    = hour
-
-
+    def clean_hour_and_minutes(self) :
+        self._hour   += self._minutes / 60
+        self._minutes = self._minutes % 60
+        self._hour   %= 24

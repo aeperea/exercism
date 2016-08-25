d9b7c78bab788fcc685f04314976a6b9a9a33ef4
Alejandro Perea <elizondoperea@gmail.com>
2016-08-25 02:30:57 -0500

    Wayyy better solution

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -1,25 +1,12 @@
 defmodule Raindrops do
-  # I'm very ashamed of this code :(
-  def convert(number), do: convert(number, "", [3, 5, 7])
-
-  defp convert(number, str, mods) do
-    if rem(number, 3) == 0 and Enum.member?(mods,3) do
-      convert(number, str <> "Pling", mods |> Enum.filter_map(&(&1 != 3), &(&1)))
+  def convert(num) do
+    result = get_word(num, 3, "i") <> get_word(num, 5, "a") <> get_word(num, 7, "o")
+    if result != "" do
+      result
     else
-      if rem(number, 5) == 0 and Enum.member?(mods,5) do
-        convert(number, str <> "Plang", mods |> Enum.filter_map(&(&1 != 5), &(&1)))
-      else
-        if rem(number, 7) == 0 and Enum.member?(mods,7) do
-          convert(number, str <> "Plong", mods |> Enum.filter_map(&(&1 != 7), &(&1)))
-        else
-          if str == "" do
-            number |> Integer.to_string
-          else
-            str
-          end
-        end
-      end
+      to_string num
     end
   end
 
+  defp get_word(n, mod, letter), do: rem(n, mod) == 0 && "Pl" <> letter <> "ng" || ""
 end

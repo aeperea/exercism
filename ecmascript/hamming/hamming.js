7b007ac0389c42bc82ffc747cbb1489bfdab0c63
aeperea <elizondoperea@gmail.com>
2016-09-10 18:04:22 -0500

    Solve for hamming in es6

---
+++ ecmascript/hamming/hamming.js
@@ -0,0 +1,12 @@
+export default class Hamming {
+  compute(str1, str2){
+    if (str1.length !== str2.length) {
+      throw new Error('DNA strands must be of equal length.');
+    }
+    let mismatches = 0;
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] !== str2[i]) { mismatches++; }
+    }
+    return mismatches;
+  }
+}

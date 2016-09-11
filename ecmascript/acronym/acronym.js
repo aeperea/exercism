a9f058e152f2e30b646f6d5402374933a857577d
aeperea <elizondoperea@gmail.com>
2016-09-11 18:34:49 -0500

    Sovling with replace regex when lowercase char is next to upercase char

--- ecmascript/acronym/acronym.js
+++ ecmascript/acronym/acronym.js
@@ -1,18 +1,7 @@
-export default class Acrynoms {
-  static upcaseInside(w) {
-    let matches = w.match(/[A-Z]/g);
-    if (matches && matches.length > 1) {
-      return {ok: true, matches: matches.join("")};
-    }
-    return {ok: false, matches: null};
-  }
-  static parse(str) {
-    let separatedWords = str.replace(/[,-:]/, " ").split(/\W+/);
-    return separatedWords.map(word => {
-      if (word.toUpperCase() === word) {return word[0].toUpperCase();}
-      let {ok, matches} = this.upcaseInside(word);
-      if (ok) { return matches; }
-      return word[0].toUpperCase();
-    }).join("");
-  }
+export default {
+  parse: (str) => str
+    .replace(/([a-z])([A-Z])/g, '$1 $2') 
+    .split(/[\s\-]/)
+    .map(w => w[0].toUpperCase())
+    .join('')
 }
--- ecmascript/acronym/babel-output/acronym.js
+++ ecmascript/acronym/babel-output/acronym.js
@@ -1,53 +1,13 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Acrynoms = (function () {
-  function Acrynoms() {
-    _classCallCheck(this, Acrynoms);
+exports['default'] = {
+  parse: function parse(str) {
+    return str.replace(/([a-z])([A-Z])/g, '$1 $2').split(/[\s\-]/).map(function (w) {
+      return w.substr(0, 1).toUpperCase();
+    }).join('');
   }
-
-  _createClass(Acrynoms, null, [{
-    key: "upcaseInside",
-    value: function upcaseInside(w) {
-      var matches = w.match(/[A-Z]/g);
-      if (matches && matches.length > 1) {
-        return { ok: true, matches: matches.join("") };
-      }
-      return { ok: false, matches: null };
-    }
-  }, {
-    key: "parse",
-    value: function parse(str) {
-      var _this = this;
-
-      var separatedWords = str.replace(/[,-:]/, " ").split(/\W+/);
-      return separatedWords.map(function (word) {
-        if (word.toUpperCase() === word) {
-          return word[0].toUpperCase();
-        }
-
-        var _upcaseInside = _this.upcaseInside(word);
-
-        var ok = _upcaseInside.ok;
-        var matches = _upcaseInside.matches;
-
-        if (ok) {
-          return matches;
-        }
-        return word[0].toUpperCase();
-      }).join("");
-    }
-  }]);
-
-  return Acrynoms;
-})();
-
-exports["default"] = Acrynoms;
-module.exports = exports["default"];
+};
+module.exports = exports['default'];

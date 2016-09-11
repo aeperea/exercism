4bee1ddea450d72989cb58f77bf295d51d26b47d
aeperea <elizondoperea@gmail.com>
2016-09-10 19:37:12 -0500

    Solved without alphabet counter

--- ecmascript/pangram/babel-output/pangram.js
+++ ecmascript/pangram/babel-output/pangram.js
@@ -1,52 +1,42 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var Pangram = (function () {
-  function Pangram(phrase) {
+  function Pangram() {
+    var phrase = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
+
     _classCallCheck(this, Pangram);
 
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-    this.phrase = phrase.toLowerCase();
-    this.letterCounter = [].concat(_toConsumableArray(alphabet)).reduce(function (o, v) {
-      o[v] = 0;return o;
-    }, {});
+    this.alphabet = [].concat(_toConsumableArray('abcdefghijklmnopqrstuvwxyz'));
+    this.phrase = [].concat(_toConsumableArray(phrase.toLowerCase()));
   }
 
   _createClass(Pangram, [{
-    key: "updateLetterCounts",
-    value: function updateLetterCounts() {
+    key: 'isPangram',
+    value: function isPangram() {
       var _this = this;
 
-      [].concat(_toConsumableArray(this.phrase)).forEach(function (letter) {
-        if (typeof _this.letterCounter[letter] !== "undefined") {
-          _this.letterCounter[letter]++;
-        }
-      });
-    }
-  }, {
-    key: "isPangram",
-    value: function isPangram() {
       if (this.phrase.length === 0) {
         return false;
       }
-      this.updateLetterCounts();
 
       var noZero = true;
-      Object.values(this.letterCounter).forEach(function (counts) {
-        if (counts === 0) {
+      this.alphabet.forEach(function (letter) {
+        if (!_this.phrase.includes(letter)) {
           noZero = false;
+          return noZero;
         }
       });
+
       return noZero;
     }
   }]);
@@ -54,5 +44,5 @@ var Pangram = (function () {
   return Pangram;
 })();
 
-exports["default"] = Pangram;
-module.exports = exports["default"];
+exports['default'] = Pangram;
+module.exports = exports['default'];
--- ecmascript/pangram/pangram.js
+++ ecmascript/pangram/pangram.js
@@ -1,25 +1,19 @@
 export default class Pangram {
-  constructor(phrase){
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-    this.phrase = phrase.toLowerCase();
-    this.letterCounter = [...alphabet].reduce((o,v) => {o[v] = 0; return o;}, {})
-  }
-  updateLetterCounts() {
-    [...this.phrase].forEach(letter => {
-      if (typeof this.letterCounter[letter] !== "undefined") {
-        this.letterCounter[letter]++;
-      }
-    });
+  constructor(phrase = ''){
+    this.alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+    this.phrase   = [...phrase.toLowerCase()];
   }
   isPangram(){
     if (this.phrase.length === 0) {return false;}
-    this.updateLetterCounts();
 
     let noZero = true;
-    Object.values(this.letterCounter).forEach(counts => {
-      if (counts === 0) { noZero = false; }
-    })
+    this.alphabet.forEach(letter => {
+      if(!this.phrase.includes(letter)) {
+        noZero = false;
+        return noZero;
+      }
+    });
+
     return noZero;
   }
 }

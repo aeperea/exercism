4547063ff9cabb17b336c7ef2c73e9b27cf42b40
aeperea <elizondoperea@gmail.com>
2016-09-13 00:16:56 -0500

    Rearranged code

--- ecmascript/trinary/trinary.js
+++ ecmascript/trinary/trinary.js
@@ -2,15 +2,14 @@ export default class Trinary {
   constructor(n){
     this.n = n;
   }
-  isTrinary(n){
-    let re = /^[012]+$/;
-    return re.test(n);
-  }
   toDecimal(){
     if (!this.isTrinary(this.n)) { return 0; }
-
     return [...this.n].reverse().reduce((o,v,i) => {
       return o + Number(v) * Math.pow(3,i);
     }, 0);
+  }
+  isTrinary(n){
+    let re = /^[012]+$/;
+    return re.test(n);
   }
 }

cfa1d8725db9df08428dca51ef64d684dc2f50bd
aeperea <elizondoperea@gmail.com>
2016-09-14 02:34:12 -0500

    Solving with super regex

--- ecmascript/pig-latin/babel-output/pig-latin.js
+++ ecmascript/pig-latin/babel-output/pig-latin.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var PigLatin = (function () {
   function PigLatin() {
@@ -14,42 +14,16 @@ var PigLatin = (function () {
   }
 
   _createClass(PigLatin, [{
-    key: "translate",
+    key: 'translate',
     value: function translate(phrase) {
-      var _this = this;
-
-      return phrase.split(" ").map(function (word) {
-        if (/[aeiouy]/.test(word[0])) {
-          return word + 'ay';
-        }
-        return _this.separateConsonant(word);
-      }).join(" ");
-    }
-  }, {
-    key: "separateConsonant",
-    value: function separateConsonant(word) {
-      var newWord = '';
-      var lastLetter = '';
-
-      for (var i = 0; i < word.length; i++) {
-        var letter = word[i];
-        if (/[^aeiouy]/.test(letter)) {
-          newWord += letter;
-        } else {
-          lastLetter = newWord[newWord.length - 1];
-          if (letter === 'u' && lastLetter === 'q') {
-            newWord += letter;
-          } else {
-            break;
-          }
-        }
-      }
-      return word.split(newWord)[1] + newWord + 'ay';
+      return phrase.replace(/\w+/g, function (word) {
+        return word.replace(/((?:qu|[^aeiou])*)(.*)/, '$2$1ay');
+      });
     }
   }]);
 
   return PigLatin;
 })();
 
-exports["default"] = PigLatin;
-module.exports = exports["default"];
+exports['default'] = PigLatin;
+module.exports = exports['default'];
--- ecmascript/pig-latin/pig-latin.js
+++ ecmascript/pig-latin/pig-latin.js
@@ -1,31 +1,7 @@
 export default class PigLatin {
-
-  translate(phrase){
-    return phrase.split(" ").map(word => {
-      if (/[aeiouy]/.test(word[0])) {
-        return word + 'ay';
-      }
-      return this.separateConsonant(word);
-    }).join(" ");
-  }
-
-  separateConsonant(word) {
-    let newWord    = '';
-    let lastLetter = '';
-
-    for (let i = 0 ; i < word.length ; i++) {
-      let letter = word[i];
-      if (/[^aeiouy]/.test(letter)) {
-        newWord += letter;
-      } else {
-        lastLetter = newWord[newWord.length - 1];
-        if (letter === 'u' && lastLetter === 'q') {
-          newWord += letter;
-        } else {
-          break;
-        }
-      }
-    }
-    return word.split(newWord)[1] + newWord + 'ay';
+  translate(phrase) {
+    return phrase.replace(/\w+/g,
+      word => word.replace(
+          /((?:qu|[^aeiou])*)(.*)/, '$2$1ay'));
   }
 }

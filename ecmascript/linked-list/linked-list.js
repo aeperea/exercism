9cc72b6b679810a6e697d911b23703d72fb90e76
aeperea <elizondoperea@gmail.com>
2016-09-16 02:01:22 -0500

    Better method description

--- ecmascript/linked-list/linked-list.js
+++ ecmascript/linked-list/linked-list.js
@@ -7,10 +7,16 @@ const Node = (val, prev = null, next = null) => {
 };
 
 const LinkedList = () => {
+  // [1,2,3,4]
+  // head = 1, tail = 4
+  //
   let head = null,
       tail = null,
       size = 0;
 
+  // push action 
+  // [1,2,3,4] => [1,2,3,4,x]
+  //
   const push = (n) => {
     head
       ? tail = tail.next = new Node(n, tail, null)
@@ -18,11 +24,14 @@ const LinkedList = () => {
     size++;
   }
 
+  // pop action 
+  // [1,2,3,4] => [1,2,3]
+  //
   const pop = () => {
-    if (size === 0) {return undefinded;}
+    if (size === 0) {return undefined;}
 
     let val = tail.val;
-    if (tail.prev) {tail.prev.next = null;}
+    if (tail.prev) {tail.prev.next = null;} //tail.prev podría ser null si tail == head
     tail = tail.prev;
     if (!tail) {head = null;}
     size--;
@@ -30,6 +39,9 @@ const LinkedList = () => {
     return val;
   }
 
+  // unshift action 
+  // [1,2,3,4] => [x,1,2,3,4]
+  //
   const unshift = (val) => {
     head
       ? head = head.prev = new Node(val, null, head)
@@ -37,10 +49,13 @@ const LinkedList = () => {
     size++;
   }
 
+  // shift action 
+  // [1,2,3,4] => [2,3,4]
+  //
   const shift = () => {
     if (size === 0) return undefined;
 
-    const val = head.val;
+    let val = head.val;
     if (head.next) head.next.prev = null;
     head = head.next;
     if (!head) tail = null;

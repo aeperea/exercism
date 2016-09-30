c47b5b84d0a7508be98bc3dec5cd10ad7e608306
aeperea <elizondoperea@gmail.com>
2016-09-30 01:27:55 -0500

    Solution using any? and filter functions

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -1,15 +1,7 @@
 defmodule SumOfMultiples do
   def to(limit, factors) do
-    factors
-    |> Enum.map(fn(fact) ->
-      if fact < limit do
-        Enum.to_list(fact..limit-1)
-        |> Enum.map(fn(v) -> if rem(v, fact) == 0, do: v, else: nil end)
-      end
-    end)
-    |> List.flatten
-    |> Enum.reject(fn(x) -> is_nil(x) == true end)
-    |> Enum.uniq
+    1..(limit - 1)
+    |> Enum.filter(fn(n) -> Enum.any?(factors, fn(fact) -> rem(n, fact) == 0 end) end)
     |> Enum.sum
   end
 end

d710b6d0f6992055f2addbcd7f0504cdc74582a8
aeperea <elizondoperea@gmail.com>
2016-09-29 23:57:04 -0500

    Solution for sublist considering that the elements must be consecutive

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,15 +1,24 @@
 defmodule Sublist do
   def compare(a, b) do
-    # a in b - :sublist
-    # b in a - :superlist
-    # :equal, :unequal
-    Enum.map(a, fn(v) -> Enum.member?(b,v))
-    # if all matches -> sublist
-    # if not, check with b in a
-    # if all matches -> superlist
-
-
-
-
+    cond do
+      a === b      -> :equal
+      inside(a, b) -> :superlist
+      inside(b, a) -> :sublist
+      true         -> :unequal
+    end
   end
+
+  defp inside(a,b) do
+    size_a = Enum.count(a)
+    size_b = Enum.count(b)
+
+    cond do
+      size_b > size_a -> false
+      Enum.take(a, size_b) === b -> true
+      true ->
+        [_ | tail] = a
+        inside(tail, b)
+    end
+  end
+
 end

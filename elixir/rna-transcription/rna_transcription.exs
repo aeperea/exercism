6c02752d285169688679e9ca9668155faf328d45
aeperea <elizondoperea@gmail.com>
2016-10-01 15:54:07 -0500

    Solution with small func

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -1,9 +1,8 @@
 defmodule RNATranscription do
-
-  @dna_maps %{67 => 71, 71 => 67, 65 => 85, 84 => 65}
+  @dna_maps %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
 
   def to_rna(dna) do
-    dna |> Enum.map(fn(nucl) ->(if @dna_maps[nucl], do: @dna_maps[nucl], else: nucl) end)
+    dna |> Enum.map(&(@dna_maps[&1]))
   end
 
 end

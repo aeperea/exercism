3289c563443f20c86eb5ce837583068a8d328184
aeperea <elizondoperea@gmail.com>
2016-10-02 17:16:15 -0500

    :) :) :)

---
+++ elixir/nth-prime/nth_prime.exs
@@ -0,0 +1,23 @@
+defmodule Prime do
+
+  def nth(goal), do: get_nth_prime(2, 1, goal)
+
+  defp is_prime(n) do
+    if n <= 3 do
+      true
+    else
+      last = :math.sqrt(n) |> Float.floor |> round
+      not (2..last |> Enum.any?(&(rem(n, &1) == 0)))
+    end
+  end
+
+  defp get_nth_prime(_, _, goal) when goal < 1, do: raise "error"
+
+  defp get_nth_prime(num, goal, goal), do: num
+
+  defp get_nth_prime(num, count, goal) do
+    new_count = if is_prime(num + 1), do: count + 1, else: count
+    get_nth_prime(num + 1, new_count, goal)
+  end
+
+end

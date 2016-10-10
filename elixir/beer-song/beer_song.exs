40445ff1ae2b10a08288106f888784dfa073f274
aeperea <elizondoperea@gmail.com>
2016-10-10 15:36:30 -0500

    Solution for beer song in elixir

---
+++ elixir/beer-song/beer_song.exs
@@ -0,0 +1,33 @@
+defmodule BeerSong do
+  def verse(3) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(number) do
+    """
+    #{number - 1} bottles of beer on the wall, #{number - 1} bottles of beer.
+    Take one down and pass it around, #{number - 2} bottles of beer on the wall.
+    """
+  end
+
+  def lyrics(range \\ 100..1) do
+    Enum.map_join(range, "\n", &verse/1)
+  end
+end
